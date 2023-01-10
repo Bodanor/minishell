a23e78091cd5c4f2de8857909a4189cd46201ac4
Maxime Finette <mfinette@z4r11p3.42lyon.fr>
2023-01-10 10:36:39 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 10:07:01 by mfinette          #+#    #+#             */
+/*   Updated: 2023/01/10 10:23:58 by mfinette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define MAX_LINE_LENGTH 256
+
+void execute_command(char* command) {
+    char* cmd = strtok(command, " ");
+    char* arg = strtok(NULL, " ");
+
+    if (strcmp(cmd, "cd") == 0) {
+        if (arg == NULL) {
+            fprintf(stderr, "cd: missing operand\n");
+            return;
+        }
+        if (chdir(arg) != 0) {
+            perror("cd");
+        }
+    } else if (strcmp(cmd, "mkdir") == 0) {
+        if (arg == NULL) {
+            fprintf(stderr, "mkdir: missing operand\n");
+            return;
+        }
+        if (mkdir(arg, 0777) != 0) {
+            perror("mkdir");
+        }
+    } else if (strcmp(cmd, "cat") == 0) {
+        if (arg == NULL) {
+            fprintf(stderr, "cat: missing operand\n");
+            return;
+        }
+        pid_t pid = fork();
+        if (pid == 0) {
+            // child process
+            execlp(cmd, cmd, arg, NULL);
+            printf("Command not found\n");
+            exit(1);
+        } else if (pid > 0) {
+            // parent process
+            int status;
+            wait(&status);
+        } else {
+            // fork failed
+            perror("Fork failed");
+            exit(1);
+        }
+    } else if (strcmp(cmd, "echo") == 0) {
+        if (arg == NULL) {
+            printf("\n");
+            return;
+        }
+        printf("%s\n", arg);
+    } else {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // child process
+            execlp(command, command, NULL);
+            printf("Command not found\n");
+            exit(1);
+        } else if (pid > 0) {
+            // parent process
+            int status;
+            wait(&status);
+        } else {
+            // fork failed
+            perror("Fork failed");
+            exit(1);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char command[MAX_LINE_LENGTH];
+
+    while (1) {
+        printf("minishell> ");
+        fgets(command, MAX_LINE_LENGTH, stdin);
+
+        // remove the newline character from the input
+        command[strcspn(command, "\n")] = 0;
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+
+        execute_command(command);
+    }
+
+    return 0;
+}
