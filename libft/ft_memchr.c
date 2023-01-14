9f677d3ab5a09a03fbb9213055984f5ccf34e7e0
Bodanor <christos.papadopoulos@student.hepl.be>
2023-01-14 20:22:12 +0100

    Fix warning that caused gcc to abord compilation because of "-Werror" flag

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -15,12 +15,13 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
-
+	const unsigned char *s_cast = (const unsigned char*)s;
+	
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return (&((void *)s)[i]);
+		if (s_cast[i] == (unsigned char)c)
+			return (void*)&s_cast[i];
 		i++;
 	}
 	return (0);
